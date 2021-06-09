60a23cfb6b60888147a869f05fd80a3679a9e9a2
InfinityRahul <78150833+InfinityRahul@users.noreply.github.com>
2021-06-09 22:42:58 +0530

    Update VInDecoder.py

--- VInDecoder.py
+++ VInDecoder.py
@@ -22,6 +22,7 @@ st.title("""
         
 ##pytesseract.pytesseract.tesseract_cmd = 'C:\\Users\\rahulbhatt\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe'
 ##pytesseract.pytesseract.tesseract_cmd = ‘/app/.apt/usr/bin/tesseract’
+pytesseract.pytesseract.tesseract_cmd = ./.apt/usr/share/tesseract-ocr/4.00/tessdata
 
 file = st.file_uploader("Please upload an image of the relevant category", type=["jpg", "png","JPEG"])
 
